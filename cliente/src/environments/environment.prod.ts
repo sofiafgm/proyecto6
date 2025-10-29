b6f25a3b6cb7c6b999951044b889e3d2b28907f3
Sofia Gomez <sofiafgm@gmail.com>
2025-10-29 12:11:48 -0600

    updated

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://proyecto6back.onrender.com/'
+    apiUrl: 'https://proyecto6back.onrender.com'
   };
--- servicios/server.js
+++ servicios/server.js
@@ -31,5 +31,5 @@ app.use('/usuarios', usuariosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en ${PORT}`);
 });
